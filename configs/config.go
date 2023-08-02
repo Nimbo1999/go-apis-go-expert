f023bb96c022cf07c080bdcea26a8d2caf837dcd
Matheus Lopes <matlopes1999@gmail.com>
2023-08-01 22:49:22 -0300

    Install viper package and add the config file to load environment variables

---
+++ configs/config.go
@@ -0,0 +1,38 @@
+package configs
+
+import (
+	"github.com/go-chi/jwtauth"
+	"github.com/spf13/viper"
+)
+
+var cfg *conf
+
+type conf struct {
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBHost        string `mapstructure:"DB_HOST"`
+	DBPort        string `mapstructure:"DB_PORT"`
+	DBName        string `mapstructure:"DB_NAME"`
+	DBUser        string `mapstructure:"DB_USER"`
+	DBPassword    string `mapstructure:"DB_PASSWORD"`
+	WebServerPort string `mapstructure:"WEB_SERVER_PORT"`
+	JWTSecret     string `mapstructure:"JWT_SECRET"`
+	JWTExpiresIn  int    `mapstructure:"JWT_EXPIRES_IN"`
+	TokenAuth     *jwtauth.JWTAuth
+}
+
+func LoadConfig(path string) (*conf, error) {
+	viper.SetConfigName("app_config")
+	viper.SetConfigType("env")
+	viper.SetConfigFile(".env")
+	viper.AddConfigPath(path)
+	// It gives priority to your environment virables instead of .env file.
+	viper.AutomaticEnv()
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+	err = viper.Unmarshal(&cfg)
+	if err != nil {
+		panic(err)
+	}
+}
